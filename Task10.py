633e869e10990e23d2c6a3bfe975d11a13b870ee
M0r1u5 <kuki.amakuki@gmail.com>
2019-11-16 16:03:01 +0600

    1

---
+++ Task10.py
@@ -0,0 +1,41 @@
+string = input('Enter any word: ')
+
+# if len(string) >= 3:
+#     print(string[2])
+# else:
+#     print('The word should has at least 3 character!')
+#
+# print(string[1:-1])
+
+# if len(string) >= 5:
+#     print(string[:5])
+# else:
+#     print('The word should has at least 5 character!')
+#
+# if len(string) >= 2:
+#     print(string[:-2])
+# else:
+#     print('The word should has at least 2 character!')
+#
+length = len(string)
+#
+lon = []
+
+# for i in range(0, length):
+#     if i % 2 == 0:
+#         lon.append(i)
+# print(lon)
+# #
+#
+# if len(string) >= 2:
+#     print(string[1::2])
+# else:
+#     print('The word should has at least 2 character!')
+#
+# reverse = string[::-1]
+#
+# print(reverse)
+#
+# print(reverse[::2])
+#
+# print(length)
